98bad2559b8db73f7fc6d172285088c8409e130e
Javier Alonso Lledo <61024432+javierxxal@users.noreply.github.com>
2020-11-18 13:20:03 +0100

    Revert "Colas y Pilas"
    This reverts commit c2c04ab5d68582f0f9f0a24402ad899b7eae348a.

--- GestionWeb/main.cpp
+++ GestionWeb/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include "Nodo.h"
-#include "Pila.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hola pepe" << endl;
-    Pila p;
-    p.apilar(3);
-    p.apilar(4);
-    cout << p.mostrarCima() << endl;
-    p.desapilar();
-    cout << p.mostrarCima() << endl;
+    cout << 1+2 << endl;
     return 0;
 }
